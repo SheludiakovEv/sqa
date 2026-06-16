739e4639075f083cbb1e249748c0d9083ec242f6
Shelu <sheludiakovev@gmail.com>
2026-06-17 01:56:24 +0500

    Лекция 8.2

--- new_montis_tests/src/test/java/ru/stqa/montis/manager/ApplicationManager.java
+++ new_montis_tests/src/test/java/ru/stqa/montis/manager/ApplicationManager.java
@@ -1,4 +1,50 @@
 package ru.stqa.montis.manager;
 
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.io.StringReader;
+import java.util.Properties;
+
 public class ApplicationManager {
+
+    private WebDriver driver;
+    private String string;
+    private Properties properties;
+    private SessionHelper sessionHelper;
+
+    public void init(String browser, Properties properties) {
+        this.string = browser;
+        this.properties = properties;
+    }
+
+    public WebDriver driver() {
+        if (driver == null){
+            if("firefox".equals(string)){
+                driver = new FirefoxDriver();
+            } else if ("chrome".equals(string)){
+                driver = new ChromeDriver();
+            } else if ("safari".equals(string)) {
+                driver = new SafariDriver(); // Доступно только для МакОс
+            } else {
+                throw new IllegalArgumentException(String.format("Не тот браузер %s", string));
+            }
+
+            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
+
+            driver.get(properties.getProperty("web.baseUrl"));
+            driver.manage().window().setSize(new Dimension(550, 693));
+        }
+        return driver;
+    }
+
+    public SessionHelper session(){
+        if (sessionHelper == null){
+            sessionHelper = new SessionHelper(this);
+        }
+        return sessionHelper;
+    }
 }
