7ae510180ca3d4f7f361ff0a4fb0b685c14c46bb
Shelu <sheludiakovev@gmail.com>
2026-06-18 18:25:56 +0500

    Лекция 8.5

--- new_montis_tests/src/test/java/ru/stqa/montis/manager/ApplicationManager.java
+++ new_montis_tests/src/test/java/ru/stqa/montis/manager/ApplicationManager.java
@@ -17,6 +17,7 @@ public class ApplicationManager {
     private SessionHelper sessionHelper;
     private HttpSessionHelper httpSessionHelper;
     private JamesCliHelper jamesCliHelper;
+    private MailHelper mailHelper;
 
     public void init(String browser, Properties properties) {
         this.string = browser;
@@ -66,5 +67,12 @@ public class ApplicationManager {
             jamesCliHelper = new JamesCliHelper(this);
         }
         return jamesCliHelper;
+    }
+
+    public MailHelper mail() {
+        if (mailHelper == null){
+            mailHelper = new MailHelper(this);
+        }
+        return mailHelper;
     }
 }
